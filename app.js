addc8d71d47c1690219e22250f9d6d391eb5db06
mromiario <mromiario@gmail.com>
2019-09-21 16:50:01 +0700

    fiximage

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express()
 const routes = require('./routes/')
 const port = process.env.PORT || 3000
  
-app.use(express.urlencoded({extended:false}))
-app.use(express.json())
+app.use(express.urlencoded({ limit: '50mb', extended: false }))
+app.use(express.json({ limit: '50mb' }))
 app.use(cors()) 
 app.use('/',routes)
  
